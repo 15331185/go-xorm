80dda547a99686200bc166d181abd3c131708c37
15331185 <383599743@qq.com>
2017-11-29 22:52:38 +0800

    Update initial.go

--- entities/initial.go
+++ entities/initial.go
@@ -2,18 +2,21 @@ package entities
 
 import (
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/go-xorm/core"
 	"github.com/go-xorm/xorm"
 )
 
 var engine *xorm.Engine
 
 func init() {
-	// Init xorm engine
-	Engine, err := xorm.NewEngine("mysql", "root:root@tcp(127.0.0.1:3306)/test?charset=utf8&parseTime=true")
-	checkErr(err)
-	err = Engine.Sync2(new(UserInfo))
-	checkErr(err)
-	engine = Engine
+	//创建orm引擎
+	var err error
+	engine, err = xorm.NewEngine("mysql", "root:root@tcp(127.0.0.1:3306)/test?charset=utf8&parseTime=true")
+	if err != nil {
+		panic(err)
+	}
+	//设置名称映射规则
+	engine.SetMapper(core.SameMapper{})
 }
 
 func checkErr(err error) {
