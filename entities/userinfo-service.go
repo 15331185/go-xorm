c01ee9f34215c59e9744bb59cac0242dbc17b026
15331185 <383599743@qq.com>
2017-11-29 22:54:02 +0800

    Update userinfo-service.go

--- entities/userinfo-service.go
+++ entities/userinfo-service.go
@@ -6,31 +6,18 @@ type UserInfoAtomicService struct{}
 //UserInfoService .
 var UserInfoService = UserInfoAtomicService{}
 
+/********全都由Dao层实现***********/
 // Save .
 func (*UserInfoAtomicService) Save(u *UserInfo) error {
-	_, err := engine.Insert(u)
-	checkErr(err)
-	return nil
+	return Save(u)
 }
 
 // FindAll .
 func (*UserInfoAtomicService) FindAll() []UserInfo {
-	rows, err := engine.Rows(new(UserInfo))
-	defer rows.Close()
-	checkErr(err)
-	bean := new(UserInfo)
-	var uList []UserInfo
-	for rows.Next() {
-		err = rows.Scan(bean)
-		uList = append(uList, *bean)
-	}
-	return uList
+	return FindAll()
 }
 
 // FindByID .
 func (*UserInfoAtomicService) FindByID(id int) *UserInfo {
-	u := new(UserInfo)
-	_, err := engine.ID(id).Get(u)
-	checkErr(err)
-	return u
+	return FindByID(id)
 }
